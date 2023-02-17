68e5d10262ce9330d2af047d1e8b4343f9c2cd38
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-17 14:14:01 -0800

    using sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - program to print size of int char and so on
- * Return:  (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -12,9 +12,9 @@ int main(void)
 	float e;
 	printf("size of a char: %lu  byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of an int: %lu  byte(s)\n", (unsigned long)sizeof(b));
-	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
 
