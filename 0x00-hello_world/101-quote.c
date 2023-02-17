80d94f11c2b036b063b0379ca4352581bbfd464a
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-17 15:36:26 -0800

    8th task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - start of program
+ * Return:  sucessful
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+i}
