0192c94b5b233dc3288d91b6023e078bb9bf8c72
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-17 13:25:04 -0800

    using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - a program that uses the prinf function
+ *Return: 0 (success)
+*/
+int main(void)
+{
+	printf("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
