e053f11038400d777c6af6008247e720a5dfc391
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-18 07:20:05 -0800

    combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - program starts
+ *
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c > 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+
+
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - code starts here
+ *
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i--)
+
