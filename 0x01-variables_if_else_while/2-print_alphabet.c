fcd4b0eee3a7d01a6c64df0bcba1dd9edb5587af
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-17 18:00:08 -0800

    task 1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - start of c code
+ *
+ * Return: 0 program is successful
+ */
+int main(void)
+{
+	for (i = "a"; i <= "z", i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
