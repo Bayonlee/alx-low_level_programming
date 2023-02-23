39422664b78aaf93ec5d55456f25065cad86b267
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-23 09:47:44 -0800

    check for lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks for lower case char
+ * c - alphabet to be checked
+ * Return: 1 for lowercase and 0 for uppercase
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+		else
+			return (0);
+}
