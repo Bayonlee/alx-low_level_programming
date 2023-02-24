94f188525bb808385bcad3cd37fa3f8ec0b52d5a
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-24 04:46:53 -0800

    absolute numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - print sugn function
+ * Return: 1 if positive
+ * and 0 if zero
+ * and -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		printf('+');
+	return (1);
+	else if (n = 0)
+		printf('0');
+	return (0);
+	else
+		printf('-');
+	return (-1);
+}
