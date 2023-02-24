394f484c73ab80d0b7bbe8b5ff55e83a0f77200d
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-24 06:38:33 -0800

    addition of two numbers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,17 @@
 #include "main.h"
 /**
+ * add - function to add two integers
+ *
+ * @b: parameter b
+ * @a: parameter a
+ * Return: 0 successful
  */
 int add(int a, int b)
 {
 	int c;
+
 	c = a + b;
+
 	return (c);
 }
 
