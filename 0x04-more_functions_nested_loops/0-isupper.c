10ddd7ff92a7d4237d94997db6343b01b6f06456
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-24 08:37:20 -0800

    uppwrcase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - function to check uppercase
+ * Return: 1 if upper and 0 if lower 
+ */
+int _isupper(int c)
+{
+	if (c >=65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
