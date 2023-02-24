607ef153d0b23afc6acc99a18b2bd65a2f77a137
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-24 05:07:53 -0800

    printing last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit -  function to print last digit
+ * @a: parameter to be checked
+ * @b: storing the formular
+ * Return: b if successful
+ *
+ */
+int print_last_digit(int a)
+
+{
+	int b;
+
+	b = a % 10;
+	if (a < 0)
+		b = -b;
+	_putchar(b +'0');
+	return (b);
+}
+
