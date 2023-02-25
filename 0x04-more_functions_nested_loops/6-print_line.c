9c21ffce7c4b0828c3b1ef58341ccc74ae18797d
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-25 11:50:26 -0800

    draw a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - function to print line
+ * @n: no of lines to print
+ */
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
