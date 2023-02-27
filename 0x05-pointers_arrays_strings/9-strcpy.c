553d73d717dcc3f62ba0ac19b4212425c054bc8b
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-02-27 05:49:04 -0800

    last task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcpy - function the copy the str to the dest
+ * @dest: where the string is copied to
+ * @src: string to be copied
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
