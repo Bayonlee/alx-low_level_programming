301ecc90e0a2ef6ff770780f11146f859cb361a6
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-02 10:20:53 -0800

    compare

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strcmp -  function to compare two srrings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
