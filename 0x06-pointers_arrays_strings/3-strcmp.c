037a00f8956b2584cd65038c87998b0c589ce076
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-02 10:53:01 -0800

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,24 +2,22 @@
 /**
  * _strcmp -  function to compare two srrings
  * @s1: first string to be compared
- * @s2: second string to be compared
+ * @s2: second string to be compare
+ * Return: 0
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
 		{
-			if (s1[i] < s2[i])
-				return (s1[i] -s2[i]);
-			else if (s1[i]  > s2[i])
-				return (s1[i] -s2[i]);
+			return (s1[i] - s2[i]);
 		}
-			else
-				return (0);
+		i++;
 	}
 	return (0);
 }
+
