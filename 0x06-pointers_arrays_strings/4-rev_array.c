5bfb45c7de4364bba3c8b54b98bcfe5aef053728
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-02 10:58:50 -0800

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,19 @@
-#include "main.h
+#include "main.h"
+
+/**
+ * reverse_array - function to reverse string
+ * @a:string
+ * @n: string
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int b;
+
+	for (i = 0; i < n--; i++)
+	{
+		b = a[i];
+		a[i] = a[n];
+		a[n] = b;
+	}
+}
