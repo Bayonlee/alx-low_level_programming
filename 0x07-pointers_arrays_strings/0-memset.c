5c3a6c0dd3f1a50c50c77174617d3819e4ce96fb
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-06 04:15:10 -0800

    memory with constant byte

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - function to fill memory with constant byte
+ * @s: memory area to be returned
+ * @b: constanr byte
+ * @n: size of byte
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	while (i < n)
+	{
+		*(s + i) = b;
+		i += 1;
+	}
+	return (s);
+}
