f9955475190e73e4ebaf029b2e3447aeccb83ec2
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-08 03:43:57 -0800

    length of string😍😍😍

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to print length of a string
+ * @s: string to be checked
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+	return (length);
+}
