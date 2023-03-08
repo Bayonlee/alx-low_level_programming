b1e346454174e183575f53efbb3ad83b32b634e4
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-08 09:58:31 -0800

    print 1px alphabet

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,37 @@
 #include <stdio.h>
 /**
  * print_to_98 - function to iterate from n to 98
+ * @n: number to itrate from
  * Return: 0
  */
 void print_to_98(int n)
 {
 	int i;
-	
-	for (i = n; i <= 98; i++)
-	_putchar(i + '0');
-	_putchar(',');
-	_putchar(' ');
+
+	if (n >= 0 || n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d, ", n);
 }
