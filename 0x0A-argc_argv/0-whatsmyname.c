a3ade700cef49250bea6bf11df1d5cfbe74069a5
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-15 04:25:17 -0700

    mynameis - name of file is the first ik the argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument counter
+ * @argv: arguments in an array
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
