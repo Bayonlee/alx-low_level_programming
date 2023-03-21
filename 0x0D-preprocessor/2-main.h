f91707d68bf0c6a015514f8c28acae4a0d3a4a04
Ajisafe Emmanuel <ajisafe.bayo2019@gmail.com>
2023-03-21 15:21:41 -0700

    function like macro

---
+++ 0x0D-preprocessor/2-main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - program to print name of file
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef func_lyk_macro_h
+#define func_lyk_macro_h
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif
